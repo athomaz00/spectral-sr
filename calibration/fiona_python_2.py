1d9e82eee21b9b987f8f25ba8c251cbfb9e9f1d5
Alexandre de Thomaz <athomaz@illinois.edu>
2017-12-22 17:01:15 -0600

    Calcutation for all colors/fitting peaks/calibration curve

--- calibration/fiona_python_2.py
+++ calibration/fiona_python_2.py
@@ -4,12 +4,16 @@ Created on Sat Aug 15 12:32:33 2015
 
 @author: Andre
 """
+import seaborn as sns
+sns.set()
 import matplotlib.pyplot as plt
 import numpy as np
 from mpl_toolkits.mplot3d import Axes3D
 import scipy.optimize as sp
+from scipy.spatial import distance
 
 
+plt.close('all')
 #####################################################################################
 def twoD_Gaussian(x_y, offset, amplitude, xo, yo, sigma_x, sigma_y, theta):
     x, y = x_y
@@ -21,56 +25,83 @@ def twoD_Gaussian(x_y, offset, amplitude, xo, yo, sigma_x, sigma_y, theta):
     g = offset + amplitude*np.exp( - (a*((x-xo)**2) + 2*b*(x-xo)*(y-yo) 
                             + c*((y-yo)**2)))
     return g.ravel()
+
 ###################################################################################
     
 
-image = plt.imread('580.tif') #read image file
+imagesFiles = [ '488.tif',  '592.tif', '680.tif', '735.tif']
 
 
+initialPoint = np.array([[243, 214], [244, 254], [247,282], [248,296]])
 
-
-initialPoint = [232, 252]
+zeroPoint = 680
 
 
 #############################################
-squareSize = 16
 
-#for some reason X,Y in the image is inverted check this out later
-X, Y = np.mgrid[initialPoint[0]-squareSize/2:initialPoint[0]+squareSize/2 +1,initialPoint[1]-squareSize/2:initialPoint[1]+squareSize/2 +1]
+centerPos = []
 
-imageMask =  image[int(np.min(Y)):int(np.max(Y))+1, int(np.min(X)):int(np.max(X))+1] #to mask it correctly we have to invert X and Y here
+for i,items in enumerate(imagesFiles):
+    
+    image = plt.imread(items) #read image file
+    
+    squareSize = 16
+    
+    X, Y = np.mgrid[initialPoint[i][0]-squareSize/2:initialPoint[i][0]+squareSize/2 +1,initialPoint[i][1]-squareSize/2:initialPoint[i][1]+squareSize/2 +1]
+    
+    imageMask =  image[int(np.min(Y)):int(np.max(Y))+1, int(np.min(X)):int(np.max(X))+1] #to mask it correctly we have to invert X and Y here
+    
+    ig = (np.min(image), np.max(image) , initialPoint[i][0], initialPoint[i][1], 2.0, 2.0, 0.0)
+    inpars, pcov = sp.curve_fit(twoD_Gaussian,(X,Y), imageMask.ravel(), p0=ig, maxfev=20000)
+    fit3 = twoD_Gaussian((X, Y), *inpars).reshape(len(X),len(Y))
+    
+    
+    #Plot image
+    fig1 = plt.figure()
+    ax1 = fig1.add_subplot(111, projection='3d')
+    ax1.plot_surface(X,Y,imageMask,rstride=1, cstride=1, cmap="hsv", linewidth=0, antialiased=False, alpha=0.5)
+    ax1.plot_wireframe(X,Y,fit3, color="black", linewidth=3, alpha=0.3)
+    ax1.set_xlabel("Pixels")
+    ax1.set_ylabel("Pixels")
+    ax1.set_zlabel("Intensity")
+    plt.title(items)
+    
+       
+    # Getting center and standard deviation in x and y
+    xCenter = inpars[2] #*PixSize      # Center of x in nanometer
+    yCenter = inpars[3] #*PixSize       # Center of y in nanometer
+    print('==========================')
+    print(items)
+    print('X pos: ' + str(xCenter))
+    print('Y pos: ' + str(yCenter))
+    print('===========================')
+    
+    splitName = items.split('.tif')
+    centerPos.append([int(splitName[0]), xCenter, yCenter])
 
-ig = (np.min(image), np.max(image) , initialPoint[0], initialPoint[1], 2.0, 2.0, 0.0)
-inpars, pcov = sp.curve_fit(twoD_Gaussian,(X,Y), imageMask.ravel(), p0=ig, maxfev=20000)
-fit3 = twoD_Gaussian((X, Y), *inpars).reshape(len(X),len(Y))
+centerPos = np.array(centerPos)
+
+distancesAll = distance.cdist(centerPos[:,1:3], centerPos[:,1:3])
+
+pixelRef_ind = np.where(centerPos[:,0] == zeroPoint)[0][0]
+
+distanceCenter = distancesAll[pixelRef_ind, :]
+
+distanceCenter[0:pixelRef_ind] = distanceCenter[0:pixelRef_ind]*-1
+
+plt.figure() 
+plt.plot(distanceCenter, centerPos[:,0], 'o')
+plt.xlabel('Pixel Displacement')
+plt.ylabel('Wavelength (nm)')
+plt.title('Pixel Displacement by Wavelength')
 
 
-#Plot image
-fig1 = plt.figure()
-ax1 = fig1.add_subplot(111, projection='3d')
-ax1.plot_surface(X,Y,imageMask,rstride=1, cstride=1, cmap="hsv", linewidth=0, antialiased=False, alpha=0.5)
-ax1.plot_wireframe(X,Y,fit3, color="black", linewidth=3, alpha=0.3)
-ax1.set_xlabel("Pixels")
-ax1.set_ylabel("Pixels")
-ax1.set_zlabel("Intensity")
+z = np.polyfit(distanceCenter, centerPos[:,0], 3)
+p = np.poly1d(z)
 
-
-# Getting center and standard deviation in x and y
-xCenter = inpars[2] #*PixSize      # Center of x in nanometer
-yCenter = inpars[3] #*PixSize       # Center of y in nanometer
-print('X pos: ' + str(xCenter))
-print('Y pos: ' + str(yCenter))
-
-# Obtaining b
-#residual = image-fit3;                  # Extract residual
-#fig2 = plt.figure()
-#ax2 = fig2.add_subplot(111, projection='3d')
-#ax2.plot_surface(X,Y,residual,rstride=1, cstride=1, cmap="hsv", linewidth=1, antialiased=False)
-##residual = residual<0
-#b = np.sqrt(sum(np.ravel(residual)*np.ravel(residual))/(len(np.ravel(residual))-1))
-
-
-
+xx = np.arange(np.min(distanceCenter)-2, np.max(distanceCenter)+2, 0.5)
+yy = p(xx)
+plt.plot(xx,yy, '--r')
 
 
 
