cae3b71fd8f15bc128d049a4409951a1bcb28f5c
Alexandre de Thomaz <athomaz@illinois.edu>
2017-12-28 16:31:09 -0600

    finished calculation for 2 colors

--- beads/wavelength_calculation.py
+++ beads/wavelength_calculation.py
@@ -37,27 +37,25 @@ im_red = Image.open(imagesFiles[1])
 im_green = Image.open(imagesFiles[2])
 
 #Mean factor for tresholding (everything less than treshold is zero)
-mean_factor = 1.7
+
 image = np.array(image)
-#img_mean_base = np.mean(image)
-#
-#msk = image<(img_mean_base*mean_factor)
-#image[msk] = 0.0
+
 
 #Check treshold, otherwise it may find more than one peak
-thr_base = np.max(image)-30*np.std(image)
+thr_base = np.max(image)-32*np.std(image)
 peaks_base = peak_local_max(image, min_distance=10, threshold_abs=thr_base)
 peaks_base[:,0], peaks_base[:,1] = peaks_base[:,1], peaks_base[:,0].copy()
 
 
 delete_base = []
 for i,points in enumerate(peaks_base):
-    if points[1]-30<0:
+    if points[1]-40<0:
         delete_base.append(i)
     if 512-points[0]<15:
         delete_base.append(i)
 peaks_base = np.delete(peaks_base, delete_base, 0)
 print(str(peaks_base.shape[0]) + ' 680 peaks')
+
 #plt.figure()
 #plt.imshow(image)
 #plt.grid('off')
@@ -65,11 +63,7 @@ print(str(peaks_base.shape[0]) + ' 680 peaks')
 
 ##############################################################################
 im_red = np.array(im_red)
-#mean_factor = 1.7
-#img_mean_base = np.mean(im_red)
 
-#msk = im_red<(img_mean_base*mean_factor)
-#im_red[msk] = 0.0
 
 thr_red = np.max(im_red)-18*np.std(im_red)
 peaks_red = peak_local_max(im_red, min_distance=10, threshold_abs=thr_red)
@@ -95,12 +89,7 @@ print(str(peaks_red.shape[0]) + ' red peaks')
 
 #############################################################################
 im_green = np.array(im_green)
-#mean_factor = 1.8
-#img_mean_base = np.mean(im_green)
 
-#msk = im_green<(img_mean_base*mean_factor)
-
-#im_green[msk] = 0.0
 thr_green = np.max(im_green)-16.5*np.std(im_green)
 peaks_green = peak_local_max(im_green, min_distance=10, threshold_abs=thr_green)
 peaks_green[:,0], peaks_green[:,1] = peaks_green[:,1], peaks_green[:,0].copy()
@@ -135,36 +124,44 @@ print(str(peaks_green.shape[0]) + ' green peaks')
 #############################################
 base_centers = np.empty((0,2))
 base_centers_trans = np.empty((0,2))
-sumBase = np.empty((21,peaks_base.shape[0]))
+sumBase = np.empty((61,peaks_base.shape[0]))
 
 for i, peaks in enumerate(peaks_base):
-    squareSize = 20
-    #peaks = peaks_base
     
-    X, Y = np.mgrid[peaks[0]-squareSize/2:peaks[0]+squareSize/2 +1,peaks[1]-squareSize/2:peaks[1]+squareSize/2 +1]
+    squareSize = 80
     
-    imageMask =  image[int(np.min(Y)):int(np.max(Y))+1, int(np.min(X)):int(np.max(X))+1] #to mask it correctly we have to invert X and Y here
-    imageMaskcopy =  np.copy(image)[int(np.min(Y)):int(np.max(Y))+1, int(np.min(X)):int(np.max(X))+1]
+    xmin = peaks[0] - 10
+    xmax = peaks[0] + 10
+    ymin = peaks[1] - 40
+    ymax = peaks[1] + 20
+    
+    x = np.arange(xmin, xmax,1)
+    y = np.arange(ymin, ymax+1,1)
+    
+    X, Y = np.meshgrid(x,y)
+    
+    imageMask =  image[int(np.min(y)):int(np.max(y))+1, int(np.min(x)):int(np.max(x))+1] #to mask it correctly we have to invert X and Y here
+    imageMaskcopy =  np.copy(image)[int(np.min(y)):int(np.max(y))+1, int(np.min(x)):int(np.max(x))+1]
     sumBaseTemp = np.sum(imageMaskcopy, axis=1)
     sumBase[:,i] = sumBaseTemp
   
     
-    ig = (np.min(image), np.max(image) , peaks[1], peaks[0], 2.0, 2.0, 0.0)
+    ig = (np.min(imageMask), np.max(imageMask) , peaks[1]+0.5, peaks[0]+0.5, 2.0, 2.0, 0.0)
     inpars, pcov = sp.curve_fit(twoD_Gaussian,(Y,X), imageMask.ravel(), p0=ig, maxfev=20000)
-    fit3 = twoD_Gaussian((X, Y), *inpars).reshape(len(X),len(Y))
+    fit3 = twoD_Gaussian((Y, X), *inpars).reshape(X.shape[0], Y.shape[1])
     
     base_centers = np.append (base_centers, [[inpars[2], inpars[3]]], axis=0)
-    base_centers_trans = np.append (base_centers_trans, [[inpars[2]-peaks[1]+10, inpars[3]-peaks[0]+10]], axis=0)
+    base_centers_trans = np.append (base_centers_trans, [[inpars[2]-ymin, inpars[3]-xmin]], axis=0)
     
     #Plot image
-    #fig1 = plt.figure()
-    #ax1 = fig1.add_subplot(111, projection='3d')
-    #ax1.plot_surface(X, Y, imageMask, rstride=1, cstride=1, cmap="hsv", linewidth=0, antialiased=False, alpha=0.5)
-    #ax1.plot_wireframe(X, Y, fit3, color="black", linewidth=3, alpha=0.3)
-    #ax1.set_xlabel("Pixels")
-    #ax1.set_ylabel("Pixels")
-    #ax1.set_zlabel("Intensity")
-    # 
+#    fig1 = plt.figure()
+#    ax1 = fig1.add_subplot(111, projection='3d')
+#    ax1.plot_surface(X, Y, imageMask, cmap="hsv", linewidth=0, antialiased=False, alpha=0.5)
+#    ax1.plot_wireframe(X, Y, fit3, color="black", linewidth=3, alpha=0.3)
+#    ax1.set_xlabel("Pixels")
+#    ax1.set_ylabel("Pixels")
+#    ax1.set_zlabel("Intensity")
+     
     #plt.show()
     
     
@@ -189,14 +186,18 @@ for i, peaks in enumerate(peaks_base):
 #######################################################################################################
 red_centers = np.empty((0,2))
 red_centers_trans = np.empty((0,2))
-sumRed = np.empty((21,peaks_base.shape[0]))
+sumRed = np.empty((61,peaks_base.shape[0]))
 
 for i,peaks in enumerate(peaks_base):
-    squareSizeX = 20
-    squareSizeY = 20
-    #peaks = peaks_base
+    xmin = peaks[0] - 10
+    xmax = peaks[0] + 10
+    ymin = peaks[1] - 40
+    ymax = peaks[1] + 20
     
-    X, Y = np.mgrid[peaks[0]-squareSizeX/2:peaks[0]+squareSizeX/2 +1,peaks[1]-squareSizeY/2:peaks[1]+squareSizeY/2 +1]
+    X, Y = np.mgrid[xmin:xmax+1:, ymin:ymax+1]
+    
+    
+
     
     redMask =  im_red[int(np.min(Y)):int(np.max(Y))+1, int(np.min(X)):int(np.max(X))+1] #to mask it correctly we have to invert X and Y here
     redMaskcopy =  np.copy(im_red)[int(np.min(Y)):int(np.max(Y))+1, int(np.min(X)):int(np.max(X))+1]
@@ -240,37 +241,18 @@ for i,peaks in enumerate(peaks_base):
 
 #######################################################################################################
 green_centers = np.empty((0,2))
-sumGreen = np.empty((41,peaks_base.shape[0]))
+sumGreen = np.empty((61,peaks_base.shape[0]))
 
 for i,peaks in enumerate(peaks_base):
-    squareSizeX = 20
-    squareSizeY = 40
-#    dx = quareSize/2
-#    dy = quareSize/2
+    
+    xmin = peaks[0] - 10
+    xmax = peaks[0] + 10
+    ymin = peaks[1] - 40
+    ymax = peaks[1] + 20
+    
+    X, Y = np.mgrid[xmin:xmax+1:, ymin:ymax+1]
+    
 
-    
-    #peaks = peaks_base
-    
-    #peakDispY = peaks[1]-30
-    
-#    if peaks[0]>dx:
-#        min_x = peaks[0] - dx
-#    else:
-#        min_x=0                                    #If point is close to left border
-#    if (peaks[0]+dx)>=np.shape(image)[0]: #if point is close to right border
-#        max_x = np.shape(image)[0]-1
-#    else:
-#        max_x = peaks[0] + dx
-#    if peakDispY>dy:
-#        min_y = peakDispY - dy
-#    else:
-#        min_y = 0
-#    if (peakDispY+dy)>=np.shape(image)[1]:
-#        max_y = np.shape(image)[1]-1
-#    else:
-#        max_y = peakDispY + dy
-    
-    X, Y = np.mgrid[peaks[0]-squareSizeX/2:peaks[0]+squareSizeX/2 +1,peaks[1]-squareSizeY/2:peaks[1]+squareSizeY/2 +1]
     
     greenMask = im_green[int(np.min(Y)):int(np.max(Y))+1, int(np.min(X)):int(np.max(X))+1] #to mask it correctly we have to invert X and Y here
     
@@ -285,6 +267,15 @@ for i,peaks in enumerate(peaks_base):
 #    #fit3 = twoD_Gaussian((X, Y), *inpars).reshape(len(X),len(Y))
 #    
 #    green_centers = np.append (green_centers, [[inpars[2], inpars[3]]], axis=0)
+    
+#X, Y = np.mgrid[peaks_base[-1][0]-squareSizeX/2:peaks_base[-1][0]+squareSizeX/2 +1,peaks_base[-1][1]-squareSizeY/2:peaks_base[-1][1]+squareSizeY/2 +1]
+#    
+#greenMask = im_green[int(np.min(Y)):int(np.max(Y))+1, int(np.min(X)):int(np.max(X))+1] #to mask it correctly we have to invert X and Y here
+#    
+#greenMaskcopy =  np.copy(im_green)[int(np.min(Y)):int(np.max(Y))+1, int(np.min(X)):int(np.max(X))+1]
+#sumGreenTemp = np.sum(greenMaskcopy, axis=1)
+#sumGreen[:,i] = sumGreenTemp
+#    
 #
 #plt.figure()
 #plt.imshow(im_green)
@@ -292,8 +283,11 @@ for i,peaks in enumerate(peaks_base):
 #plt.scatter(base_centers[3,0], base_centers[3,1], facecolor='none', edgecolor="white")
 
 #########################################################################################################
-coef = np.array([8e-5,0.0197,3.4768, 680])
+coef = np.array([8e-5,0.0197,3.7468, 680])
 p = np.poly1d(coef)
+sumGreen[30:-1,:] = 0
+sumRed[0:30,:] = 0
+sumBase[0:30,:] = 0
 
 def smooth(y, box_pts):
     box = np.ones(box_pts)/box_pts
@@ -301,32 +295,44 @@ def smooth(y, box_pts):
     return y_smooth
 
 spectralMeanTableRed = np.empty((np.shape(base_centers_trans)[0],1))
-#spectralMeanTableGreen = np.empty((np.shape(base_centers_trans)[0]+20,2))
+spectralMeanTableGreen = np.empty((np.shape(base_centers_trans)[0],1))
+spectralMeanTableBase = np.empty((np.shape(base_centers_trans)[0],1))
 
 for i, centers in enumerate(base_centers_trans):
-    pixels = np.arange(0,21,1)
-    x_center = centers[0] 
-    y_center = centers[1]
+    pixels = np.arange(0,61,1)
+    x_center = centers[1] 
+    y_center = centers[0]
     pixels_x = pixels - x_center
     pixels_y = pixels - y_center
     pixel_disp = p(pixels_y)
-    pixel_disp_new = np.linspace(pixel_disp.min(), pixel_disp.max(), 300)
+
    
     sumNormRed = (sumRed[:,i]) /np.max(sumRed[:,i]) -0.7
     sumNormGreen = (sumGreen[:,i]) /np.max(sumGreen[:,i]) -0.7
     sumNormBase = (sumBase[:,i])/np.max(sumBase[:,i]) 
+    
     spectralMeanRed = np.sum(np.multiply(sumRed[:,i],pixel_disp))/np.sum(sumRed[:,i])
-    #spectralMeanGreen = np.sum(np.multiply(sumGreen[:,i],pixel_disp-30))/np.sum(sumGreen[:,i])
+    
+    spectralMeanGreen = np.sum(np.multiply(sumGreen[:,i],pixel_disp))/np.sum(sumGreen[:,i])
+    
+    spectralMeanBase = np.sum(np.multiply(sumBase[:,i],pixel_disp))/np.sum(sumBase[:,i])
+    
+    
     spectralMeanTableRed[i,0] = spectralMeanRed
-    #spectralMeanTableGreen[i,1] = spectralMeanGreen
+    spectralMeanTableGreen[i,0] = spectralMeanGreen
+    spectralMeanTableBase[i,0] = spectralMeanBase
+    
+    
     plt.figure(1)
-    plt.scatter([680.0],spectralMeanRed )
-   # plt.scatter([580.0],spectralMeanGreen )
-    plt.xticks(np.arange(560,720,10))
-    plt.ylim(570,720)
+#    plt.scatter([680.0],spectralMeanRed )
+#    plt.scatter([580.0],spectralMeanGreen )
+    plt.xticks(np.arange(560,720,20))
+    plt.yticks(np.arange(560,720,20))
+    plt.xlim(560,720)
+    plt.ylim(560,720)
 #    
 #    if i%1 ==  0:
-    plt.figure(2)
+    #plt.figure(2)
 #    #plt.plot(pixel_disp, sumNormRed, 'r')
 #    plt.plot(pixel_disp, smooth(sumNormRed, 3))
     #plt.plot(pixel_disp, smooth(sumNormBase, 3), 'b')
@@ -335,9 +341,15 @@ for i, centers in enumerate(base_centers_trans):
 #    
 
 plt.figure(1)
-#plt.scatter([680.0], np.mean(spectralMeanTable), c='black', s=30)
+plt.errorbar([580.0], np.mean(spectralMeanTableGreen),yerr=np.std(spectralMeanTableGreen), marker='o', markersize=3)
 plt.errorbar([680.0], np.mean(spectralMeanTableRed), yerr=np.std(spectralMeanTableRed), marker='o', markersize=3)
-    
-
-
+plt.errorbar([680.0], np.mean(spectralMeanTableBase), yerr=np.std(spectralMeanTableBase), marker='o', markersize=3)
+plt.plot([580,580], [580, np.mean(spectralMeanTableGreen)], '--k')    
+plt.plot([560,580], [ np.mean(spectralMeanTableGreen), np.mean(spectralMeanTableGreen)], '--k')   
+plt.plot([670,690], [ 680, 680], '--k')   
+plt.plot([560,600], [ 590, 590], 'g') 
+plt.xticks(np.arange(560,750,20))
+plt.yticks(np.arange(560,750,20))
+plt.xlim(560,750)
+plt.ylim(560,750)
 
