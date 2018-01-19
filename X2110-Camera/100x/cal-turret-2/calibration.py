Corrected Beads Calculation and Calibration Round
No 100% what was wrong with beads calculation, but after some debugging it seems it is calculating the spectra all right. Although, peaks are a little off from the nominal peak value. Probably the code was selecting the fitted gaussian peaks for the 680nm channel and this added a lot of variation to the image mask and this jiggled the peaks of the red and green channels.

X2110-Camera/100x/cal-turret-2/calibration.py
@@ -43,7 +43,7 @@ def twoD_Gaussian(x_y, offset, amplitude, xo, yo, sigma_x, sigma_y, theta):
 imagesFiles = [ '488.tif',  '568.tif', '680.tif', '735.tif']
 
 
-initialPoint = np.array([[243, 325], [243, 355], [243,384], [243,390]])
+initialPoint = np.array([[348, 240], [348, 275], [348,307], [348,318]])
 
 zeroPoint = 680 #point for 0 pixel displacement
 
@@ -56,7 +56,7 @@ for i,items in enumerate(imagesFiles):
     
     image = plt.imread(items) #read image file
     
-    squareSize = 16
+    squareSize = 20
     
     xmin = initialPoint[i][0]-squareSize/2
     xmax = initialPoint[i][0]+squareSize/2